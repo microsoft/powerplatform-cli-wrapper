692afee4d986c7a1303e4b726f9f7e0675b4d12c
Tyler Hogsett <tyhogset@microsoft.com>
2021-03-15 22:50:26 -0700

    make it pretty

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,9 @@ const lint = require("./gulp/lint");
 const test = require("./gulp/test");
 const restore = require("./gulp/restore");
 
-exports.clean = clean
-exports.compile = compile
-exports.recompile = recompile
+exports.clean = clean;
+exports.compile = compile;
+exports.recompile = recompile;
 exports.restore = restore;
 exports.lint = lint;
 exports.test = test;
