3c0021ec1add08ec1569f73b979d21b170fb8ccd
Tyler Hogsett <tyhogset@microsoft.com>
2021-03-19 22:12:55 -0700

    set up publish workflow and fixed file reference

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ export { Logger } from "./Logger";
 export { RunnerError } from "./CommandRunner";
 
 // TODO: delete exports once all actions are converted:
-export { createGitRunner, GitRunner } from "./gitRunner";
+export { createGitRunner, GitRunner } from "./GitRunner";
 export {
   createPacRunner,
   PacRunner,
   CdsEnvironment,
   ClientCredentials,
   UsernamePassword,
-} from "./pacRunner";
-export { createSopaRunner, SopaRunner } from "./sopaRunner";
+} from "./PacRunner";
+export { createSopaRunner, SopaRunner } from "./SopaRunner";
