4a617c07c93caabeda1063987d4cbcd5bd169051
Paul Breuler <4356863+paulbreuler@users.noreply.github.com>
2021-04-08 18:13:49 -0400

    Implement tests for export action and refactored interfaces
    Co-authored-by: melody-universe<tyler-hogsett-microsoft@users.noreply.github.com>

--- src/actions/exportSolution.ts
+++ src/actions/exportSolution.ts
@@ -1,18 +1,13 @@
 import { authenticateEnvironment } from "../pac/auth/authenticate";
-import CredentialsParameters from "../pac/auth/CredentialsParameters";
-import EnvironmentUrlParameters from "../pac/auth/EnvironmentUrlParameters";
 import createPacRunner from "../pac/createPacRunner";
-import RunnerParameters from "../RunnerParameters";
+
 import {
   exportSolution,
   ExportSolutionParameters,
 } from "../pac/exportSolution";
 
 export default async function (
-  parameters: RunnerParameters &
-    CredentialsParameters &
-    EnvironmentUrlParameters &
-    ExportSolutionParameters
+  parameters: ExportSolutionParameters
 ): Promise<void> {
   const pac = createPacRunner(parameters);
   await authenticateEnvironment(pac, parameters);
--- src/pac/exportSolution.ts
+++ src/pac/exportSolution.ts
@@ -1,15 +1,22 @@
 import { CommandRunner } from "../CommandRunner";
+import RunnerParameters from "../RunnerParameters";
+import CredentialsParameters from "./auth/CredentialsParameters";
+import EnvironmentUrlParameters from "./auth/EnvironmentUrlParameters";
 
-export interface ExportSolutionParameters {
-  name: string;
-  path: string;
+export interface ExportSolutionParameters
+  extends CredentialsParameters,
+    RunnerParameters,
+    EnvironmentUrlParameters {
+  actionParameters: {
+    name: string;
+    path: string;
+  };
 }
 
 export function exportSolution(
   pac: CommandRunner,
-  { name, path }: ExportSolutionParameters
+  { actionParameters: { name, path } }: ExportSolutionParameters
 ): Promise<string[]> {
   // Handle export parameters
-
   return pac("solution", "export", "--name", name, "--path", path);
 }
