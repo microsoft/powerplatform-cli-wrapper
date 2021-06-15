2344ff20eb7c49947ed7fb890575f71dac8d4074
david@DAVID-PC <david@davidjenni.com>
2021-06-15 15:08:34 -0700

    update dist

--- dist/CommandRunner.js
+++ dist/CommandRunner.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66d8d758a634b6827c2412890923b5f58282c5d24bda112f365df81a6ae790ab
-size 3009
+oid sha256:0dc6f59f7252044fd6b73c7ce83ddf615ebe7ed3721c5637daf95c3a4d117bd1
+size 3043
