15cbab6779a35b04e8352fb19b6dbd86e252469d
Praveen Yelleswarapu <vyelleswarap@microsoft.com>
2021-07-09 20:20:06 -0700

    Addressed code review comments

--- src/actions/deleteEnvironment.ts
+++ src/actions/deleteEnvironment.ts
@@ -3,24 +3,22 @@ import createPacRunner from "../pac/createPacRunner";
 import { RunnerParameters } from "../Parameters";
 import { AuthCredentials } from "../pac/auth/authParameters";
 
-export interface DeleteEnvironmentParameters
-{
+export interface DeleteEnvironmentParameters {
   credentials: AuthCredentials;
   environmentUrl?: string;
   environmentId?: string;
   async?: boolean;
 }
 
-export async function deleteEnvironment(parameters: DeleteEnvironmentParameters, runnerParameters: RunnerParameters): Promise<void> 
-{
+export async function deleteEnvironment(parameters: DeleteEnvironmentParameters, runnerParameters: RunnerParameters): Promise<void> {
   const pac = createPacRunner(runnerParameters);
   await authenticateAdmin(pac, parameters.credentials);
-  const pacArgs = ["admin", "delete"];
 
-  if(parameters.environmentUrl) { pacArgs.push("--url", parameters.environmentUrl);}
-  else if(parameters.environmentId) { pacArgs.push("--environment-id", parameters.environmentId); }
-  else { throw new Error("Please provide either environment id or environment url");}
-  if(parameters.async) { pacArgs.push("--async"); }
+  const pacArgs = ["admin", "delete"];
+  /** Caller needs to validate at the client level if both environment id and url are passed. */
+  if (parameters.environmentUrl) { pacArgs.push("--url", parameters.environmentUrl); }
+  if (parameters.environmentId) { pacArgs.push("--environment-id", parameters.environmentId); }
+  if (parameters.async) { pacArgs.push("--async"); }
 
   await pac(...pacArgs);
 }
