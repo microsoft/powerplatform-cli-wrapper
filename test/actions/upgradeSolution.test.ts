ddcd267d107e984f1a6e8e8a3f41c14d6704ebcd
penguy-ms <71346301+penguy-ms@users.noreply.github.com>
2021-07-15 13:50:31 -0700

    Adding solution upgrade task (#37)
    * Adding solution upgrade task
    
    * Add eslint comment
    
    * Update with cosmetic changes

---
+++ test/actions/upgradeSolution.test.ts
@@ -0,0 +1,52 @@
+import rewiremock from "../rewiremock";
+import * as sinonChai from "sinon-chai";
+import * as chaiAsPromised from "chai-as-promised";
+import { should, use } from "chai";
+import { restore, stub } from "sinon";
+import { ClientCredentials, RunnerParameters } from "../../src";
+import { CommandRunner } from "../../src/CommandRunner";
+import { createDefaultMockRunnerParameters, createMockClientCredentials, mockEnvironmentUrl } from "./mockData";
+import { UpgradeSolutionParameters } from "src/actions/upgradeSolution";
+import Sinon = require("sinon");
+should();
+use(sinonChai);
+use(chaiAsPromised);
+
+describe("action: upgrade solution", () => {
+  let pacStub: CommandRunner;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let authenticateEnvStub: Sinon.SinonStub<any[],any>;
+  const mockClientCredentials: ClientCredentials = createMockClientCredentials();
+  const envUrl: string = mockEnvironmentUrl;
+  let upgradeSolutionParameters: UpgradeSolutionParameters;
+
+  beforeEach(() => {
+    pacStub = stub();
+    authenticateEnvStub = stub();
+    upgradeSolutionParameters = createUpgradeSolutionParameters();
+  })
+  afterEach(() => restore())
+
+  async function runActionWithMocks(upgradeSolutionParameters:UpgradeSolutionParameters) {
+    const runnerParameters: RunnerParameters = createDefaultMockRunnerParameters();
+    const mockedActionModule = await rewiremock.around(() => import("../../src/actions/upgradeSolution"),
+      (mock) => {
+        mock(() => import("../../src/pac/createPacRunner")).withDefault(() => pacStub);
+        mock(() => import("../../src/pac/auth/authenticate")).with({ authenticateEnvironment: authenticateEnvStub });
+      });
+    await mockedActionModule.upgradeSolution(upgradeSolutionParameters, runnerParameters);
+  }
+
+  const createUpgradeSolutionParameters = (): UpgradeSolutionParameters => ({
+    credentials: mockClientCredentials,
+    environmentUrl: envUrl,
+    name: "mock-solution-name",
+  });
+
+  it("with required params, calls pac runner with correct args", async () => {
+    await runActionWithMocks(upgradeSolutionParameters);
+
+    authenticateEnvStub.should.have.been.calledOnceWith(pacStub, mockClientCredentials, envUrl);
+    pacStub.should.have.been.calledOnceWith("solution", "upgrade", "--solution-name", "mock-solution-name");
+  });
+});
