00fb7853b12cfcc00602f1d69b95ff30b40c299d
Peterson Nguyen <penguy@microsoft.com>
2021-09-15 16:50:34 -0700

    Rm trailing space

--- src/host/IHostAbstractions.ts
+++ src/host/IHostAbstractions.ts
@@ -9,5 +9,5 @@ export interface IHostAbstractions {
 export interface HostParameterEntry {
   readonly name: string;
   readonly required: boolean;
-  readonly defaultValue?: boolean | string; 
+  readonly defaultValue?: boolean | string;
 }
