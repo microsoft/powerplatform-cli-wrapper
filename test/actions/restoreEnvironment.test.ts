1a2e4ec19c717ca07d36e4be622e1bd31e5e4994
Praveen Yelleswarapu <vyelleswarap@microsoft.com>
2021-09-16 06:29:40 -0700

    Updated restore environment

---
+++ test/actions/restoreEnvironment.test.ts
@@ -0,0 +1,86 @@
+import rewiremock from "../rewiremock";
+import * as sinonChai from "sinon-chai";
+import * as chaiAsPromised from "chai-as-promised";
+import { expect, should, use } from "chai";
+import { restore, stub } from "sinon";
+import { ClientCredentials, RunnerParameters } from "../../src";
+import { RestoreEnvironmentParameters } from "../../src/actions";
+import { CommandRunner } from "../../src/CommandRunner";
+import { createDefaultMockRunnerParameters, createMockClientCredentials, mockEnvironmentUrl } from "./mock/mockData";
+import { mockHost } from "./mock/mockHost";
+import Sinon = require("sinon");
+should();
+use(sinonChai);
+use(chaiAsPromised);
+
+describe("action: restoreEnvironment", () => {
+  let pacStub: CommandRunner;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let authenticateAdminStub: Sinon.SinonStub<any[], any>;
+  const host = new mockHost();
+  const mockClientCredentials: ClientCredentials = createMockClientCredentials();
+  const environmentUrl: string = mockEnvironmentUrl;
+  let restoreEnvironmentParameters: RestoreEnvironmentParameters;
+
+  beforeEach(() => {
+    pacStub = stub();
+    authenticateAdminStub = stub();
+    restoreEnvironmentParameters = {
+      credentials: mockClientCredentials,
+      sourceEnvironmentUrl: environmentUrl,
+      targetEnvironmentUrl: { name: "TargetEnvironmentUrl", required: true },
+      restoreLatestBackup: { name: "RestoreLatestBackup", required: false },
+      targetEnvironmentName: { name: "FriendlyName", required: false },
+    };
+  });
+  afterEach(() => restore());
+
+  async function runActionWithMocks(restoreEnvironmentParameters: RestoreEnvironmentParameters): Promise<void> {
+    const runnerParameters: RunnerParameters = createDefaultMockRunnerParameters();
+
+    const mockedActionModule = await rewiremock.around(() => import("../../src/actions/restoreEnvironment"),
+      (mock) => {
+        mock(() => import("../../src/pac/createPacRunner")).withDefault(() => pacStub);
+        mock(() => import("../../src/pac/auth/authenticate")).with({ authenticateAdmin: authenticateAdminStub });
+      });
+
+    await mockedActionModule.restoreEnvironment(restoreEnvironmentParameters, runnerParameters, host);
+  }
+
+  it("Throws valid error when Latest Backup isn't true and date and time for backup is undefined", async () => {
+    await expect(runActionWithMocks(restoreEnvironmentParameters)).to.be.rejectedWith(
+      "Either latest backup must be true or Valid date and time for backup must be provided."
+    );
+  });
+
+  it("with required inputs set by host, calls pac runner stub with correct arguments", async () => {
+    restoreEnvironmentParameters.restoreLatestBackup = { name: "RestoreLatestBackup", required: false, defaultValue: true };
+
+    await runActionWithMocks(restoreEnvironmentParameters);
+
+    authenticateAdminStub.should.have.been.calledOnceWith(pacStub, mockClientCredentials);
+    pacStub.should.have.been.calledOnceWith("admin", "restore", "--source-url", environmentUrl, "--target-url", host.targetEnvironmentUrl,
+      "--selected-backup", "latest");
+  });
+
+  it("with all inputs set by host, calls pac runner stub with correct arguments", async () => {
+    restoreEnvironmentParameters.restoreLatestBackup = { name: "RestoreLatestBackup", required: false, defaultValue: true };
+    restoreEnvironmentParameters.targetEnvironmentName = { name: "FriendlyName", required: true };
+
+    await runActionWithMocks(restoreEnvironmentParameters);
+
+    pacStub.should.have.been.calledOnceWith("admin", "restore", "--source-url", environmentUrl, "--target-url", host.targetEnvironmentUrl,
+      "--name", host.friendlyName, "--selected-backup", "latest");
+  });
+
+  it("With restore latest backup turned off and restore time stamp set, calls pac runner stub with correct arguments", async () => {
+    restoreEnvironmentParameters.restoreLatestBackup = { name: "RestoreLatestBackup", required: false };
+    restoreEnvironmentParameters.backupDateTime = { name: "RestoreTimeStamp", required: true };
+    restoreEnvironmentParameters.targetEnvironmentName = { name: "FriendlyName", required: true };
+
+    await runActionWithMocks(restoreEnvironmentParameters);
+
+    pacStub.should.have.been.calledOnceWith("admin", "restore", "--source-url", environmentUrl, "--target-url", host.targetEnvironmentUrl,
+      "--name", host.friendlyName, "--selected-backup", host.restoreTimeStamp);
+  });
+});
