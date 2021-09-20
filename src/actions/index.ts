e8ae46bc4782c47766bdf71c66d11a8f81c9d76a
Peterson Nguyen <penguy@microsoft.com>
2021-09-20 16:08:33 -0700

    Rm auto completed variable

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import exp = require("constants");
-
 export * from "./exportSolution";
 export * from "./whoAmI";
 export * from "./importSolution";
