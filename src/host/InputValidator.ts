e7308f3f5a098244b70f06e8651bd3f1d8426093
david@DAVID-PC <davidjen@microsoft.com>
2021-11-12 13:21:53 -0800

    InputValidator TODO for future change: param entries are required for all action params (independent if the action/task makes it optional

--- src/host/InputValidator.ts
+++ src/host/InputValidator.ts
@@ -18,18 +18,21 @@ export class InputValidator {
     return val;
   }
 
-  public pushInput(pacArgs: string[], property: string, params?: HostParameterEntry, callback?: (val: string) => string): void {
-    if (params !== undefined) {
-      let val = this.getInput(params);
-      if (val === undefined && params.required) {
-        throw new Error(`Required ${params.name} not set`);
+  public pushInput(pacArgs: string[], property: string, paramEntry?: HostParameterEntry, callback?: (val: string) => string): void {
+    // TODO: change action-specific ...Parameters contracts to always require the parameter definition
+    // today, we double-encode if a task/action parameter is optional in the ...Parameters interface definition, but shouldn't!
+    if (!paramEntry) {
+      return;
+    }
+    let val = this.getInput(paramEntry);
+    if (!val && paramEntry.required) {
+      throw new Error(`Required ${paramEntry.name} not set`);
+    }
+    else if (val) {
+      if (callback) {
+        val = callback(val);
       }
-      else if (val !== undefined) {
-        if (callback) {
-          val = callback(val);
-        }
-        pacArgs.push(property, val);
-      }
+      pacArgs.push(property, val);
     }
   }
 }
