5bcef72e1f0e67d87017397a8433684325817724
david@DAVID-PC <davidjen@microsoft.com>
2021-11-12 13:20:01 -0800

    remove authentication and env-url from pack/unpack-solution; actions have no connectivity

--- src/actions/packSolution.ts
+++ src/actions/packSolution.ts
@@ -3,15 +3,11 @@
 
 import { HostParameterEntry, IHostAbstractions } from "../host/IHostAbstractions";
 import { InputValidator } from "../host/InputValidator";
-import { authenticateEnvironment, clearAuthentication } from "../pac/auth/authenticate";
 import createPacRunner from "../pac/createPacRunner";
 import { RunnerParameters } from "../Parameters";
-import { AuthCredentials } from "../pac/auth/authParameters";
 import path = require("path");
 
 export interface PackSolutionParameters {
-  credentials: AuthCredentials;
-  environmentUrl: string;
   solutionZipFile: HostParameterEntry;
   sourceFolder: HostParameterEntry;
   solutionType: HostParameterEntry;
@@ -22,9 +18,6 @@ export async function packSolution(parameters: PackSolutionParameters, runnerPar
   const pac = createPacRunner(runnerParameters);
 
   try {
-    const authenticateResult = await authenticateEnvironment(pac, parameters.credentials, parameters.environmentUrl);
-    logger.log("The Authentication Result: " + authenticateResult);
-
     const pacArgs = ["solution", "pack"];
     const validator = new InputValidator(host);
 
@@ -38,8 +31,5 @@ export async function packSolution(parameters: PackSolutionParameters, runnerPar
   } catch (error) {
     logger.error(`failed: ${error.message}`);
     throw error;
-  } finally {
-    const clearAuthResult = await clearAuthentication(pac);
-    logger.log("The Clear Authentication Result: " + clearAuthResult);
   }
 }
--- test/actions/packSolution.test.ts
+++ test/actions/packSolution.test.ts
@@ -3,10 +3,10 @@ import { should, use } from "chai";
 import * as chaiAsPromised from "chai-as-promised";
 import { restore, stub } from "sinon";
 import * as sinonChai from "sinon-chai";
-import { ClientCredentials, RunnerParameters } from "../../src";
+import { RunnerParameters } from "../../src";
 import { PackSolutionParameters } from "../../src/actions";
 import rewiremock from "../rewiremock";
-import { createDefaultMockRunnerParameters, createMockClientCredentials, mockEnvironmentUrl } from "./mock/mockData";
+import { createDefaultMockRunnerParameters } from "./mock/mockData";
 import Sinon = require("sinon");
 import { IHostAbstractions } from "../../src/host/IHostAbstractions";
 import { platform } from "os";
@@ -16,8 +16,6 @@ use(chaiAsPromised);
 
 describe("action: pack solution", () => {
   let pacStub: Sinon.SinonStub<any[],any>;
-  let authenticateEnvironmentStub: Sinon.SinonStub<any[], any>;
-  let clearAuthenticationStub: Sinon.SinonStub<any[], any>;
   const zip = "./ContosoSolution.zip";
   const mockHost: IHostAbstractions = {
     name: "host",
@@ -26,13 +24,9 @@ describe("action: pack solution", () => {
   const folder = "./folder";
   const absoluteSolutionPath = (platform() === "win32") ? 'D:\\Test\\working\\ContosoSolution.zip' : '/Test/working/ContosoSolution.zip';
   const absoluteFolderPath = (platform() === "win32") ? 'D:\\Test\\working\\folder' : '/Test/working/folder';
-  const mockClientCredentials: ClientCredentials = createMockClientCredentials();
-  const environmentUrl: string = mockEnvironmentUrl;
 
   beforeEach(() => {
     pacStub = stub();
-    authenticateEnvironmentStub = stub();
-    clearAuthenticationStub = stub();
   });
   afterEach(() => restore());
 
@@ -42,26 +36,17 @@ describe("action: pack solution", () => {
     const mockedActionModule = await rewiremock.around(() => import("../../src/actions/packSolution"),
       (mock) => {
         mock(() => import("../../src/pac/createPacRunner")).withDefault(() => pacStub);
-        mock(() => import("../../src/pac/auth/authenticate")).with(
-          {
-            authenticateEnvironment: authenticateEnvironmentStub,
-            clearAuthentication: clearAuthenticationStub
-          });
       });
     const stubFnc = Sinon.stub(mockHost, "getInput");
     stubFnc.onCall(0).returns(zip);
     stubFnc.onCall(1).returns(folder);
 
-    authenticateEnvironmentStub.returns("Authentication successfully created.");
-    clearAuthenticationStub.returns("Authentication profiles and token cache removed");
     pacStub.returns("");
     await mockedActionModule.packSolution(packSolutionParameters, runnerParameters, mockHost);
   }
 
   it("calls pac runner with correct arguments", async () => {
     const packSolutionParameters: PackSolutionParameters = {
-      credentials: mockClientCredentials,
-      environmentUrl: environmentUrl,
       solutionZipFile: { name: 'SolutionInputFile', required: true },
       sourceFolder: { name: 'SolutionTargetFolder', required: true },
       solutionType: { name: 'SolutionType', required: false, defaultValue: "Unmanaged" },
@@ -69,9 +54,7 @@ describe("action: pack solution", () => {
 
     await runActionWithMocks(packSolutionParameters);
 
-    authenticateEnvironmentStub.should.have.been.calledOnceWith(pacStub, mockClientCredentials, environmentUrl);
     pacStub.should.have.been.calledOnceWith("solution", "pack", "--zipFile", absoluteSolutionPath, "--folder", absoluteFolderPath,
       "--packageType", "Unmanaged");
-    clearAuthenticationStub.should.have.been.calledOnceWith(pacStub);
   });
 });
