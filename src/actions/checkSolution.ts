fbed306aa5908115f856c901ee7f8d1f680827de
Peterson Nguyen <penguy@microsoft.com>
2022-01-14 14:45:23 -0800

    Updating solution checker to include more params

--- src/actions/checkSolution.ts
+++ src/actions/checkSolution.ts
@@ -9,36 +9,93 @@ import path = require("path");
 export interface CheckSolutionParameters {
   credentials: AuthCredentials;
   environmentUrl: string;
+  fileLocation?: HostParameterEntry;
   solutionPath: HostParameterEntry;
+  solutionUrl?: HostParameterEntry;
   geoInstance?: HostParameterEntry;
+  ruleSet?: HostParameterEntry;
   ruleLevelOverride: HostParameterEntry;
   outputDirectory: HostParameterEntry;
+  useDefaultPAEndpoint?: HostParameterEntry;
+  customPAEndpoint?: HostParameterEntry;
+  filesExcluded?: HostParameterEntry;
+  errorLevel?: HostParameterEntry;
+  errorThreshold?: HostParameterEntry;
+  failOnAnalysisError?: HostParameterEntry;
 }
 
 export async function checkSolution(parameters: CheckSolutionParameters, runnerParameters: RunnerParameters, host: IHostAbstractions): Promise<void> {
   const logger = runnerParameters.logger;
   const pac = createPacRunner(runnerParameters);
-
+  const validator = new InputValidator(host);
+  let level: string | undefined;
+  let threshold: string | undefined;
+  if (parameters.errorLevel != undefined && parameters.errorThreshold != undefined) {
+    level = validator.getInput(parameters.errorLevel);
+    threshold = validator.getInput(parameters.errorThreshold);
+  }
   try {
     const authenticateResult = await authenticateEnvironment(pac, parameters.credentials, parameters.environmentUrl);
     logger.log("The Authentication Result: " + authenticateResult);
 
     const pacArgs = ["solution", "check"]
-    const validator = new InputValidator(host);
 
-    validator.pushInput(pacArgs, "--path", parameters.solutionPath, (value) => path.resolve(runnerParameters.workingDir, value));
+    if (parameters.fileLocation != undefined && validator.getInput(parameters.fileLocation) === 'sasUriFile') {
+      validator.pushInput(pacArgs, "--solutionUrl", parameters.solutionUrl);
+    }
+    else {
+      validator.pushInput(pacArgs, "--path", parameters.solutionPath, (value) => path.resolve(runnerParameters.workingDir, value));
+    }
     validator.pushInput(pacArgs, "--geo", parameters.geoInstance);
+    validator.pushInput(pacArgs, "--ruleSet", parameters.ruleSet);
     validator.pushInput(pacArgs, "--ruleLevelOverride", parameters.ruleLevelOverride);
     validator.pushInput(pacArgs, "--outputDirectory", parameters.outputDirectory);
+    validator.pushInput(pacArgs, "--excludedFiles", parameters.filesExcluded);
+
+    if (parameters.useDefaultPAEndpoint != undefined && validator.getInput(parameters.useDefaultPAEndpoint) === 'true') {
+      validator.pushInput(pacArgs, "--customEndpoint", parameters.useDefaultPAEndpoint);
+    }
+    else {
+      validator.pushInput(pacArgs, "--customEndpoint", parameters.customPAEndpoint);
+    }
 
     logger.log("Calling pac cli inputs: " + pacArgs.join(" "));
     const pacResult = await pac(...pacArgs);
     logger.log("CheckSolution Action Result: " + pacResult);
+
+    const status = pacResult[23].split(' ')[2];
+    if (status === 'Failed' || status === 'FinishedWithErrors') {
+      throw new Error("PowerApps Checker analysis results indicate a failure or error during the analysis process.");
+    }
+    if (level != undefined && threshold != undefined) {
+      errorCheck(pacResult, level, parseInt(threshold));
+    }
   } catch (error) {
     logger.error(`failed: ${error.message}`);
     throw error;
   } finally {
     const clearAuthResult = await clearAuthentication(pac);
     logger.log("The Clear Authentication Result: " + clearAuthResult);
+  }
+}
+
+function errorCheck(pacResults: string[], errorLevel: string, errorThreshold: number): void {
+  const errors: Record<string, number> = {};
+  const PAErrorLevels = pacResults[25].trim().split(/\s+/);
+  const PAErrorValues = pacResults[27].trim().split(/\s+/);
+
+  for (let i = 0; i < PAErrorLevels.length && i < PAErrorValues.length; i++) {
+    errors[PAErrorLevels[i]] = parseInt(PAErrorValues[i]);
+  }
+  const issueCount: Record<string, string> = {
+    "CriticalIssueCount": "Critical",
+    "HighIssueCount": "High",
+    "MediumIssueCount": "Medium",
+    "LowIssueCount": "Low",
+    "InformationalIssueCount": "Informational"
+  };
+  
+  if (errors[issueCount[errorLevel]] > errorThreshold) {
+    throw new Error("Analysis results do not pass with selected error level and threshold choices.  Please review detailed results in SARIF file for more information.");
   }
 }
--- test/actions/checkSolution.test.ts
+++ test/actions/checkSolution.test.ts
@@ -18,12 +18,46 @@ describe("action: check solution", () => {
   const pacStub: Sinon.SinonStub<any[],any> = stub();
   const authenticateEnvironmentStub: Sinon.SinonStub<any[], any> = stub();
   const clearAuthenticationStub: Sinon.SinonStub<any[], any> = stub();
+  const pacResults:string[] = [
+    "Entering InvokePowerAppsChecker - EndProcessing",
+    "Executing InvokePowerAppsChecker",
+    "",
+    "Upload started : 1 files",
+    "",
+    "Uploaded finished: 1 files taking 00:00:00.2915742",
+    "",
+    "Calling status check API. Loop count 1",
+    "",
+    "Finished calling status check API. Loop count 1",
+    "",
+    "... Activity Waiting on results(1); Status: Analyzing; PercentComplete: 30",
+    "Pausing for 15 seconds.",
+    "",
+    "Calling status check API. Loop count 2",
+    "",
+    "Finished calling status check API. Loop count 2",
+    "... Activity Waiting on results(1); Status: Analyzing; PercentComplete: 100",
+    "",
+    "Downloading 1 files to C:\\Users\\penguy\\src\\GithubActions",
+    "",
+    "Finished downloading 1 files",
+    "",
+    "Exiting with Finished InvokePowerAppsChecker - EndProcessing : 00:00:16.0008479",
+    "",
+    " Critical High Medium  Low Informational ",
+    "",
+    "        0    0       2   1             0",
+    "",
+    "",
+  ]
   const zip = "./ContosoSolution.zip";
   const mockHost : IHostAbstractions = {
     name: "host",
     getInput: () => zip,
   }
   const samplejson = "samplejson";
+  const customEndpoint = "www.japan.com";
+  const fileLocation = "localFiles";
   const mockClientCredentials: ClientCredentials = createMockClientCredentials();
   const environmentUrl: string = mockEnvironmentUrl;
   const absoluteSolutionPath = (platform() === "win32") ? 'D:\\Test\\working\\ContosoSolution.zip' : '/Test/working/ContosoSolution.zip';
@@ -40,30 +74,42 @@ describe("action: check solution", () => {
           });
       });
     const stubFnc = Sinon.stub(mockHost, "getInput");
-    stubFnc.onCall(0).returns(zip);
-    stubFnc.onCall(1).returns(samplejson);
+    stubFnc.onCall(0).returns(undefined);
+    stubFnc.onCall(1).returns(undefined);
+    stubFnc.onCall(2).returns(fileLocation);
+    stubFnc.onCall(3).returns(zip);
+    stubFnc.onCall(4).returns(samplejson);
+    stubFnc.onCall(5).returns(undefined);
+    stubFnc.onCall(6).returns(undefined);
+    stubFnc.onCall(7).returns(customEndpoint);
 
     authenticateEnvironmentStub.returns("Authentication successfully created.");
     clearAuthenticationStub.returns("Authentication profiles and token cache removed");
-    pacStub.returns("");
+    pacStub.returns(pacResults);
     await mockedActionModule.checkSolution(checkSolutionParameters, runnerParameters, mockHost);
   }
 
-  const createCheckSolutionParameters = (): CheckSolutionParameters => ({
+  const createCheckSolutionParametersForTask = (): CheckSolutionParameters => ({
     credentials: mockClientCredentials,
     environmentUrl: environmentUrl,
-    solutionPath: { name: "SolutionInputFile", required: true },
+    fileLocation: { name: "FileLocation", required: false, defaultValue: "localFiles" },
+    solutionPath: { name: "FilesToAnalyze", required: false },
+    solutionUrl: { name: "FilesToAnalyzeSasUri", required: false },
     ruleLevelOverride: { name: "RuleLevelOverride", required: false },
-    geoInstance: undefined,
     outputDirectory: { name: "OutputDirectory", required: false },
+    useDefaultPAEndpoint: { name: "UseDefaultPACheckerEndpoint", required: false, defaultValue: true },
+    customPAEndpoint: { name: "CustomPACheckerEndpoint", required: false, defaultValue: "" },
+    errorLevel: { name: "ErrorLevel", required: false, defaultValue: "HighIssueCount" },
+    errorThreshold: { name: "ErrorThreshold", required: false, defaultValue: "0" },
+    failOnAnalysisError: { name: "FailOnPowerAppsCheckerAnalysisError", required: false, defaultValue: true },
   });
 
   it("required, optional, and not required input types calls pac runner stub with correct arguments", async () => {
-    await runActionWithMocks(createCheckSolutionParameters());
+    await runActionWithMocks(createCheckSolutionParametersForTask());
     
     authenticateEnvironmentStub.should.have.been.calledOnceWith(pacStub, mockClientCredentials, environmentUrl);
     pacStub.should.have.been.calledOnceWith("solution", "check", "--path", absoluteSolutionPath,
-    "--ruleLevelOverride", samplejson);
+    "--ruleLevelOverride", samplejson, "--customEndpoint", customEndpoint);
     clearAuthenticationStub.should.have.been.calledOnceWith(pacStub);
   });
 });
