bc94570c665ec37fa58e0383fa5e58574227b61c
David JENNI <david@davidjenni.com>
2022-01-21 16:41:07 -0800

    pac CLI Dec/Jan refresh (#78)
    * update pac CLI 1.11.x
    
    * update dist/
    
    Co-authored-by: david@DAVID-PC <davidjen@microsoft.com>

--- dist/actions/checkSolution.d.ts
+++ dist/actions/checkSolution.d.ts
@@ -4,9 +4,18 @@ import { AuthCredentials } from "../pac/auth/authParameters";
 export interface CheckSolutionParameters {
     credentials: AuthCredentials;
     environmentUrl: string;
+    fileLocation?: HostParameterEntry;
     solutionPath: HostParameterEntry;
+    solutionUrl?: HostParameterEntry;
     geoInstance?: HostParameterEntry;
+    ruleSet?: HostParameterEntry;
     ruleLevelOverride: HostParameterEntry;
     outputDirectory: HostParameterEntry;
+    useDefaultPAEndpoint?: HostParameterEntry;
+    customPAEndpoint?: HostParameterEntry;
+    filesExcluded?: HostParameterEntry;
+    errorLevel?: HostParameterEntry;
+    errorThreshold?: HostParameterEntry;
+    failOnAnalysisError?: HostParameterEntry;
 }
 export declare function checkSolution(parameters: CheckSolutionParameters, runnerParameters: RunnerParameters, host: IHostAbstractions): Promise<void>;
