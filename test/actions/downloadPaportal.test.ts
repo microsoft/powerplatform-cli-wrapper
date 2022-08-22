b5eac10fb8ce3f749fbc248693d717d51e75dd19
Ameya Apte <amapte@microsoft.com>
2022-08-22 19:34:17 +0530

    Add new params for paportal download task

--- test/actions/downloadPaportal.test.ts
+++ test/actions/downloadPaportal.test.ts
@@ -19,6 +19,9 @@ describe("action: download paportal", () => {
   let clearAuthenticationStub: Sinon.SinonStub<any[], any>;
   const path = "C:\\portals";
   const websiteId = "f88b70cc-580b-4f1a-87c3-41debefeb902";
+  const overwrite = "true"
+  const includeEntities = "adx_webpage, adx_webfile"
+  const excludeEntities = "adx_sitesetting"
   const mockedHost = new mockHost();
 
   const mockClientCredentials: ClientCredentials = createMockClientCredentials();
@@ -47,6 +50,10 @@ describe("action: download paportal", () => {
     const stubFnc = Sinon.stub(mockedHost, "getInput");
     stubFnc.onCall(0).returns(path);
     stubFnc.onCall(1).returns(websiteId);
+    stubFnc.onCall(2).returns(overwrite);
+    stubFnc.onCall(3).returns(includeEntities);
+    stubFnc.onCall(4).returns(excludeEntities);
+
 
     authenticateEnvironmentStub.returns("Authentication successfully created.");
     clearAuthenticationStub.returns("Authentication profiles and token cache removed");
@@ -59,6 +66,9 @@ describe("action: download paportal", () => {
     environmentUrl: envUrl,
     path: { name: "DownloadPath", required: true },
     websiteId: { name: "WebsiteId", required: true },
+    overwrite: { name: "Overwrite", required: false },
+    includeEntities: { name: "IncludeEntities", required: false },
+    excludeEntities: { name: "ExcludeEntities", required: false },
   });
 
   it("with required params, calls pac runner with correct args", async () => {
@@ -66,6 +76,14 @@ describe("action: download paportal", () => {
 
     authenticateEnvironmentStub.should.have.been.calledOnceWith(pacStub, mockClientCredentials, envUrl);
     pacStub.should.have.been.calledOnceWith("paportal", "download", "--path", path, "--websiteId", websiteId);
+    clearAuthenticationStub.should.have.been.calledOnceWith(pacStub);
+  });
+
+  it("with optional params, calls pac runner with include/exclude/overwrite args", async () => {
+    await runActionWithMocks(downloadPaportalParameters);
+
+    authenticateEnvironmentStub.should.have.been.calledOnceWith(pacStub, mockClientCredentials, envUrl);
+    pacStub.should.have.been.calledOnceWith("paportal", "download", "--path", path, "--websiteId", websiteId, "--overwrite", overwrite, "--includeEntities", includeEntities, "--excludeEntities", excludeEntities);
     clearAuthenticationStub.should.have.been.calledOnceWith(pacStub);
   });
 });
