be2b7b43997e5987f42bd51650cc0726ac9cc871
Swapnil Bhalgat <swapnilbhalgat92@gmail.com>
2023-05-05 10:58:13 -0700

    add catalog status action (#189)
    Co-authored-by: Swapnil Bhalgat <sbhalgat@microsoft.com>

---
+++ src/actions/catalogStatus.ts
@@ -0,0 +1,39 @@
+import { HostParameterEntry, IHostAbstractions } from "../host/IHostAbstractions";
+import { InputValidator } from "../host/InputValidator";
+import { authenticateAdmin, clearAuthentication } from "../pac/auth/authenticate";
+import createPacRunner from "../pac/createPacRunner";
+import { RunnerParameters } from "../Parameters";
+import { AuthCredentials } from "../pac/auth/authParameters";
+
+export interface CatalogStatusParameters {
+  credentials: AuthCredentials;
+  trackingId: HostParameterEntry;
+  requestType: HostParameterEntry;
+}
+
+export async function catalogStatus(parameters: CatalogStatusParameters, runnerParameters: RunnerParameters, host: IHostAbstractions): Promise<void> {
+  const logger = runnerParameters.logger;
+  const pac = createPacRunner(runnerParameters);
+
+  try {
+    const authenticateResult = await authenticateAdmin(pac, parameters.credentials, logger);
+    logger.log("The Authentication Result: " + authenticateResult);
+
+    // Made environment url mandatory and removed environment id as there are planned changes in PAC CLI on the parameter.
+    const pacArgs = ["catalog", "status"];
+    const validator = new InputValidator(host);
+
+    validator.pushInput(pacArgs, "--tracking-id", parameters.trackingId);
+    validator.pushInput(pacArgs, "--type", parameters.requestType);
+
+    logger.log("Calling pac cli inputs: " + pacArgs.join(" "));
+    const pacResult = await pac(...pacArgs);
+    logger.log("CatalogStatus Action Result: " + pacResult);
+  } catch (error) {
+    logger.error(`failed: ${error instanceof Error ? error.message : error}`);
+    throw error;
+  } finally {
+    const clearAuthResult = await clearAuthentication(pac);
+    logger.log("The Clear Authentication Result: " + clearAuthResult);
+  }
+}
