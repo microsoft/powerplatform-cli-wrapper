e51db1f7564ad6484030dbe929256b747d99f2a3
Crash Co∥ison ★☆☆☆☆ <3751389+tehcrashxor@users.noreply.github.com>
2023-07-12 16:59:46 -0700

    WIP

--- gulp/lib/nugetInstall.mjs
+++ gulp/lib/nugetInstall.mjs
@@ -1,7 +1,10 @@
+import path from "node:path";
 import { info } from "fancy-log";
+import fs from "node:fs";
 import fsextra from "fs-extra";
 import fetch from "node-fetch";
-import { resolve as _resolve } from "path";
+import { resolve as _resolve, resolve } from "node:path";
+import { spawnSync } from "node:child_process";
 import { Extract } from "unzip-stream";
 import binDir from "./binDir.mjs";
 import yargs from 'yargs';
@@ -50,22 +53,44 @@ export default async function nugetInstall(feed, pkg) {
   }
 
   const targetDir = _resolve(binDir, pkg.internalName);
-  info(`Extracting into folder: ${targetDir}`);
-  return new Promise((resolve, reject) => {
-    res.body
-      .pipe(Extract({ path: targetDir }))
-      .on("close", () => {
-        if (pkg.chmod) {
-          const exePath = _resolve(
-            targetDir,
-            ...pkg.chmod.split(/[\\/]/g)
-          );
-          chmod(exePath, 0o711);
-        }
-        resolve();
-      })
-      .on("error", (err) => {
-        reject(err);
-      });
-  });
+
+  if (pkg.installMethod === "unzip") {
+    info(`Extracting into folder: ${targetDir}`);
+    return new Promise((resolve, reject) => {
+      res.body
+        .pipe(Extract({ path: targetDir }))
+        .on("close", () => {
+          if (pkg.chmod) {
+            const exePath = _resolve(
+              targetDir,
+              ...pkg.chmod.split(/[\\/]/g)
+            );
+            chmod(exePath, 0o711);
+          }
+          resolve();
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
+    });
+  } else if (pkg.installMethod === "dotnet") {
+    info(`Installing into folder: ${targetDir}`);
+    return new Promise((resolve, reject) => {
+      const localNupkg = path.join(targetDir, `${packageName}.${version}.nupkg`);
+
+      res.body.pipe(fs.createWriteStream(localNupkg))
+        .on("close", () => { resolve(); })
+        .on("error", (err) => { reject(err); });
+    });
+
+
+  //   const dotnetArgs = ["tool", "install", packageName, "--version", version, "--tool-path", targetDir, "--add-source", targetDir];
+
+  //   const dotnet = spawnSync("dotnet", dotnetArgs, { stdio: "inherit" });
+  //   if (dotnet.status !== 0) {
+  //     throw new Error(`dotnet tool install failed with exit code: ${dotnet.status}`);
+  //   }
+  } else {
+    throw new Error(`Unknown nugetInstall type: ${pkg.installMethod}`);
+  }
 };
--- src/pac/createPacRunner.ts
+++ src/pac/createPacRunner.ts
@@ -1,5 +1,8 @@
-import { platform } from "os";
-import { resolve } from "path";
+import { platform } from "node:os";
+import { resolve } from "node:path";
+import { spawnSync } from "node:child_process";
+import * as glob from "glob";
+import * as fsextra from "fs-extra";
 import { CommandRunner, createCommandRunner } from "../CommandRunner";
 import { RunnerParameters } from "../Parameters";
 
@@ -7,11 +10,41 @@ export default function createPacRunner({workingDir, runnersDir, logger, agent}:
 {
   return createCommandRunner(
     workingDir,
-    platform() === "win32"
-      ? resolve(runnersDir, "pac", "tools", "pac.exe")
-      : resolve(runnersDir, "pac_linux", "tools", "pac"),
+    getPacPath(runnersDir),
     logger,
     agent,
     undefined,
   );
+}
+
+function getPacPath(runnersDir: string): string {
+  // Windows currently uses net48 version (not the dotnet tool) of pac, as it still supplies
+  // some windows-only features due to those features having full .NET Framework dependencies.
+  if (platform() === "win32") {
+    return resolve(runnersDir, "pac", "tools", "pac.exe");
+  }
+
+  const toolDirectory = resolve(runnersDir, "pac_tool");
+  const toolPath = resolve(toolDirectory, "pac");
+  if (fsextra.existsSync(toolPath)) {
+    return toolPath;
+  }
+
+  const nugetSearch = glob.sync(resolve(toolDirectory, "microsoft.powerapps.cli.tool.*.nupkg"));
+  if (nugetSearch.length === 1 && tryInstallDotnetTool(toolDirectory)) {
+    return toolPath;
+  }
+
+  return resolve(runnersDir, "pac_linux", "tools", "pac");
+}
+
+function tryInstallDotnetTool(toolsDirectory: string): boolean {
+  const dotnetArgs = ["tool", "install", "microsoft.powerapps.cli.tool", "--prerelease", "--tool-path", toolsDirectory, "--add-source", toolsDirectory];
+
+  const dotnet = spawnSync("dotnet", dotnetArgs, { stdio: "inherit" });
+  if (dotnet.status !== 0) {
+    return false;
+  }
+
+  return true;
 }
