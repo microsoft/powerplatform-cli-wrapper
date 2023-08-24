c07e191abb0d1a2112945328965584b1990258b9
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2023-08-24 16:37:36 -0700

    Remove unnecessary Auth for offline `pac solution version` command (#292)

--- src/actions/updateVersionSolution.ts
+++ src/actions/updateVersionSolution.ts
@@ -1,12 +1,9 @@
 import { HostParameterEntry, IHostAbstractions, CommonActionParameters } from "../host/IHostAbstractions";
 import { InputValidator } from "../host/InputValidator";
-import { authenticateEnvironment, clearAuthentication } from "../pac/auth/authenticate";
 import createPacRunner from "../pac/createPacRunner";
 import { RunnerParameters } from "../Parameters";
-import { AuthCredentials } from "../pac/auth/authParameters";
 
 export interface UpdateVersionSolutionParameters extends CommonActionParameters {
-  credentials: AuthCredentials;
   environmentUrl: string;
   buildVersion?: HostParameterEntry;
   revisionVersion?: HostParameterEntry;
@@ -20,9 +17,6 @@ export async function updateVersionSolution(parameters: UpdateVersionSolutionPar
   const pac = createPacRunner(runnerParameters);
 
   try {
-    const authenticateResult = await authenticateEnvironment(pac, parameters.credentials, parameters.environmentUrl, logger);
-    logger.log("The Authentication Result: " + authenticateResult);
-
     const pacArgs = ["solution", "version"]
     const validator = new InputValidator(host);
 
@@ -39,8 +33,5 @@ export async function updateVersionSolution(parameters: UpdateVersionSolutionPar
   } catch (error) {
     logger.error(`failed: ${error instanceof Error ? error.message : error}`);
     throw error;
-  } finally {
-    const clearAuthResult = await clearAuthentication(pac);
-    logger.log("The Clear Authentication Result: " + clearAuthResult);
   }
 }
