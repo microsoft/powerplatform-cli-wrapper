fa284011b2c0828a9b7057ca46355500ed075830
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2023-09-06 17:13:22 -0700

    Support for PAC installed in any location, instead of using runnersDir (#302)

--- src/Parameters.ts
+++ src/Parameters.ts
@@ -13,5 +13,16 @@ export interface TelemetryParameters
 export interface RunnerParameters extends LoggerParameters, TelemetryParameters
 {
   workingDir: string;
+
+  // Directory containing unzipped Windows and Linux PAC Nuget Packages.
+  // Expectation is that, both versions have been renamed such that
+  // linux PAC executable's path is <runnersDir>/pac_linux/tools/pac
+  // and windows PAC executable's path is <runnersDir>\pac\tools\pac.exe
+  // This should be ignored in favor of pacPath if specified.
   runnersDir: string;
+
+  // Full path to pac.exe or pac (linux/mac) executable. Used instead of runnersDir if specified,
+  // for scenarios where PAC is installed in the pipeline instead of included in the
+  // ADO VSIX or GitHub tarball/zipball
+  pacPath?: string;
 }
